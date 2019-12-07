c3916498e2e8c03dd2d51b63010897fbaf98cc5a
Alex Smirnov <alex-wd@yandex.ru>
2019-12-07 22:18:53 +0300

    Added js and css min, babel added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { config } from './modules/config'
 import AppService from './modules/app.service'
 import './modules/header.component'
+import './css/index.css'
 
 console.log('config key', config.key);
+
+const a = 5;
+console.log('a', a);
 
 const service = new AppService('Hello world');
 service.log();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -8,10 +11,36 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 4200
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin ({
+            filename: 'style.css'
+        }),
+        new MinifyPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 }
